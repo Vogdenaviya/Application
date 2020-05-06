06c10bc999e53be262f936b8637cc6ac5670967c
nata <vlad_lysenko@mail.ru>
2020-05-06 20:19:26 +0400

    с

--- ApplicationRabbit/src/main/kotlin/com/example/brokerMQ/Client.kt
+++ ApplicationRabbit/src/main/kotlin/com/example/brokerMQ/Client.kt
@@ -5,7 +5,9 @@ import com.example.jdbc.UsersDbEntityDAO
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
 
+@Component
 class Client {
 
     @Autowired
